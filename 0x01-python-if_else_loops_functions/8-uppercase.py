ac87a26ce0f7fb6a587462d29eff8ebb33ce9f48
apha-pesso <mutiuayo99@gmail.com>
2022-08-23 21:34:54 +0000

    to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) in range(97, 123):
+            print("{:c}".format(ord(str[i]) - 32), end="")
+        else:
+            print(str[i], end="")
+    print()
