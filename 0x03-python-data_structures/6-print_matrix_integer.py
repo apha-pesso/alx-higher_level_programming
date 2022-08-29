4010ad9d0547df7c112d21e205ce954dc96819c3
apha-pesso <mutiuayo99@gmail.com>
2022-08-29 22:28:50 +0000

    check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for a in range(len(matrix)):
-        for i in range(len(matrix[a])):
-            print("{:d}".format(matrix[a][i]), end=" ")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end=" ")
         print()
