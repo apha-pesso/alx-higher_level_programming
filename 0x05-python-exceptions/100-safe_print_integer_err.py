d7f39118f6f8c94777f55a126016a091d8b5b8ba
apha-pesso <mutiuayo99@gmail.com>
2022-09-12 19:27:08 +0000

    Handle all error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,8 +8,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (NameError, TypeError):
-        return False
-    except ValueError as err:
+    except (ValueError, NameError, TypeError) as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
