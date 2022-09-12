4b83e29db52ae911d4c93967aac9b42a440a6a3b
apha-pesso <mutiuayo99@gmail.com>
2022-09-12 16:44:13 +0000

    print result from finally

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,10 +4,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-
     except (ValueError, ZeroDivisionError):
         result = None
-
     finally:
         print("Inside result: {}".format(result))
         return result
