3b74fd7a5191f1f66aea889d1257ca19fd48c15b
apha-pesso <mutiuayo99@gmail.com>
2022-09-14 14:20:44 +0000

    Create square class with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Square with size """
+
+
+class Square():
+    """ Creates a square with size """
+    def __init__(self, size):
+        """ Initializes the class """
+        self.__size = size
