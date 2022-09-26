8dfebedd445e5695d12ce451dc45db1e2c7ffb1e
apha-pesso <mutiuayo99@gmail.com>
2022-09-26 14:45:55 +0000

    Basegeometry with non_defined area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Base Geometry"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        """Area method that raises error"""
+        raise Exception("area() is not implemented")
