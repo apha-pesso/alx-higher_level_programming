e2fd0521d1d5b60039074737f411e9633695ca9f
apha-pesso <mutiuayo99@gmail.com>
2022-09-26 14:58:02 +0000

    Basegeometry with value validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Base Geometry"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        """Area method that raises error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method to validate value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
