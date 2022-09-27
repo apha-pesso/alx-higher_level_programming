5548ee72fc5871d6139bb82a48eee0e764c35400
apha-pesso <mutiuayo99@gmail.com>
2022-09-27 21:21:40 +0000

    check

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,4 @@ def write_file(filename="", text=""):
     """Write to a file
     overwrite the file if it exists"""
     with open("filename", "w", encoding="utf-8") as w:
-        a = w.write(text)
-    return a
+        return w.write(text)
