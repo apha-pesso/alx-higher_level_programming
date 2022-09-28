5b6906fce42be095153b28bd9b70376df53b4dd0
apha-pesso <mutiuayo99@gmail.com>
2022-09-28 12:20:25 +0000

    Student class with methods

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Class that defines student"""
+
+
+class Student:
+    """Student class"""
+    first_name
+    last_name
+    age
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the object
+        Args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns json repr of the object"""
+        return (self.__dict__)
