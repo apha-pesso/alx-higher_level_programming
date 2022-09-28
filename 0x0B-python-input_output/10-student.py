e29993328ad81ace4cf2b334c268d429bb0c35d8
apha-pesso <mutiuayo99@gmail.com>
2022-09-28 15:01:07 +0000

    To json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Class that defines student"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize the object
+        Args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns json repr of the object"""
+        if not attrs:
+            return (self.__dict__)
+
+        new = {}
+        for att in attrs:
+            if att in self.__dict__:
+                new[att] = self.__dict__[att]
+        return new
