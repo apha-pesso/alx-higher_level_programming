deaa473758f2c48244bf3268e4f983aeaa53567c
apha-pesso <mutiuayo99@gmail.com>
2022-10-03 21:39:33 +0000

    Rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Rectangle class that inherits from Base"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        Base.__init__(self, id)
+
+    @property
+    def width(self):
+        """Returns the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the value for width"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the value for height"""
+        self.__height = value
+
+    @property
+    def set_x(self):
+        """Returns x"""
+        return self.__x
+
+    @set_x.setter
+    def set_x(self, value):
+        """Sets the value for x"""
+        self.__x = value
+
+    @property
+    def set_y(self):
+        """Returns y"""
+        return self.__y
+
+    @set_y.setter
+    def set_y(self, value):
+        """Sets the value for y"""
+        self.__y = value
