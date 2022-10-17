3d9c5ed86de246d25c3fbc45f42c664eb8371e5f
apha-pesso <mutiuayo99@gmail.com>
2022-10-17 21:43:30 +0000

    json to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,5 +20,16 @@ class Base:
     def to_json_string(list_dictionaries):
         """Returns the json representation of the list_dictionaries"""
         if (list_dictionaries is None or list_dictionaries == []):
-            return []
+            return "[]"
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes json string to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoder="utf-8") as new_file:
+            if list_obj is None:
+                new_file.write("[]")
+            else:
+                list_dicts = [item.to_dictionary() for item in list_objs]
+                new_file.write(Base.to_json_string(list_dicts))
