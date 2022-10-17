c2c50ef9531694ef0e585f9847eb0919cfbc693a
apha-pesso <mutiuayo99@gmail.com>
2022-10-17 16:08:51 +0000

    square check

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,16 +14,19 @@ class Square(Rectangle):
     @property
     def size(self):
         """Get the size for the squar"""
-        return (self.__size)
+        return (self.width)
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        """if type(value) != int:
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
         else:
             self.__size = value
+            """
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Prints preset formated display"""
