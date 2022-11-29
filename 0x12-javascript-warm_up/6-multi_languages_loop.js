0b528c7248dae5bd077247b14de0727612419bdf
apha-pesso <mutiuayo99@gmail.com>
2022-11-29 15:01:58 +0000

    $message

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,7 @@
 let name;
 const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 /* for (name = 0; name < 3; name++) {
- * console.log(lang[name]);
+ *  console.log(lang[name]);
  } */
 
 for (name in lang) { console.log(lang[name]); }
