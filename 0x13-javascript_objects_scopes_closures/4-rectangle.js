ef1616103a14828ab1368ccf2117008aeacdb38c
apha-pesso <mutiuayo99@gmail.com>
2022-11-30 13:47:23 +0000

    Add more method

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const x = [];
+      for (let j = 0; j < this.width; j++) {
+        x.push('X');
+        // process.stdout.write('X');
+        // x += 'X';
+      }
+      // if (i < (this.h - 1)) {
+      // console.log();
+      // }
+      console.log(`${x.join('')}`);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.heigth;
+    this.heigth = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.heigth *= 2;
+  }
+}
+module.exports = Rectangle;
