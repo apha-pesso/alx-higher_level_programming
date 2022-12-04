b8c2180024a0c1a31f3180586efa94ea8bd4c578
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:13:14 +0000

    check

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          password=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}'
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' \
                 ORDER BY states.id ASC".format(argv[4]))
     states = cur.fetchall()
 
