58ca21143ee9109a7e061fc84952188364deaf52
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:31:11 +0000

    Insert state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Add a new state"""
 
 import sqlalchemy
 from sqlalchemy import create_engine
@@ -6,23 +7,24 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-us = argv[1]
-pw = argv[2]
-db = argv[3]
+if __name__ == "__main__":
+    us = argv[1]
+    pw = argv[2]
+    db = argv[3]
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(us, pw, db), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
-session = Session()
-new_state = State(name="Louisiana")
-session.add(new_state)
-session.commit()
-all_state = session.query(State).filter(State.name.like("Louisiana")).first()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(us, pw, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    ali = session.query(State).filter(State.name.like("Louisiana")).first()
 
-if all_state:
-    print(all_state.id)
-else:
-    pass
+    if ali:
+        print(ali.id)
+    else:
+        pass
 
 session.close()
