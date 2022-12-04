6230b8207b8edf8ffe5d1937ac68e7301f1eb0b6
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:28:50 +0000

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Get a state now"""
 
 import sqlalchemy
 from sqlalchemy import create_engine
@@ -6,20 +7,21 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-us = argv[1]
-pw = argv[2]
-db = argv[3]
-st = argv[4]
+if __name__ == "__main__":
+    us = argv[1]
+    pw = argv[2]
+    db = argv[3]
+    st = argv[4]
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(us, pw, db), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
-session = Session()
-all_state = session.query(State).filter(State.name.like(st)).first()
-if all_state:
-    print(all_state.id)
-else:
-    print("Not found")
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(us, pw, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    all_state = session.query(State).filter(State.name.like(st)).first()
+    if all_state:
+        print(all_state.id)
+    else:
+        print("Not found")
 
-session.close()
+    session.close()
