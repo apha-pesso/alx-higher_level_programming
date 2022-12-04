3bbe32f55f160f7bbde7263ff24280643a0a9a95
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:35:02 +0000

    Update model

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
+"""Update a state"""
 
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from sys import argv
 from model_state import Base, State
 
-us = argv[1]
-pw = argv[2]
-db = argv[3]
+if __name__ == "__main__":
+    us = argv[1]
+    pw = argv[2]
+    db = argv[3]
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                       .format(us, pw, db), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
-session = Session()
-ali = session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
-session.commit()
-"""
-if all_state:
-    print(all_state.id)
-else:
-    pass
-"""
-session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(us, pw, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
+    session.commit()
+    session.close()
