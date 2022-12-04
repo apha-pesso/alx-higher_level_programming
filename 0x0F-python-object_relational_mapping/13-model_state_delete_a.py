ccde6c95df44c5f619184fb3b345018ff46e780f
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:41:18 +0000

    Delete a state

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Delete state"""
 
 import sqlalchemy
 from sqlalchemy import create_engine
@@ -6,15 +7,18 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-us = argv[1]
-pw = argv[2]
-db = argv[3]
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(us, pw, db), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
-session = Session()
-all_state = session.query(State).filter(State.name.like("%a%")).delete(synchronize_session='fetch')
+if __name__ == "__main__":
+    us = argv[1]
+    pw = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(us, pw, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    all_state = session.query(State).filter(State.name.like("%a%")) \
+        .delete(synchronize_session='fetch')
+    session.close()
 
 """
 if all_state:
@@ -23,5 +27,3 @@ if all_state:
 else:
     print()
 """
-session.commit()
-session.close()
