bb6eb41b170b7425d7cfaeb7ba0ac50fb78cc1ce
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:18:16 +0000

    Add script description and reformat

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Get all cities by state"""
 from sys import argv
 import MySQLdb
 
@@ -7,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], password=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities
-                JOIN states ON cities.state_id=states.id
+    cur.execute("SELECT cities.name FROM cities \
+                JOIN states ON cities.state_id=states.id \
                 WHERE states.name LIKE %s", (argv[4], ))
 
     lamb = []
