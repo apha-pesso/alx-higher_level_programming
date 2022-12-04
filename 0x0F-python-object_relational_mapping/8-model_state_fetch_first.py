a4998abd200c0a7cfdab99b8bceb406c3b8a157d
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:24:57 +0000

    Reformat to prevent from running on import

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""First state"""
 
 import sqlalchemy
 from sqlalchemy import create_engine
@@ -6,20 +7,21 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-us = argv[1]
-pw = argv[2]
-db = argv[3]
+if __name__ == "__main__":
+    us = argv[1]
+    pw = argv[2]
+    db = argv[3]
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(us, pw, db), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
-session = Session()
-all_state = session.query(State).first()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(us, pw, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    all_state = session.query(State).first()
 
-if all_state:
-    print("{}: {}".format(all_state.id, all_state.name))
-else:
-    print()
+    if all_state:
+        print("{}: {}".format(all_state.id, all_state.name))
+    else:
+        print()
 
-session.close()
+    session.close()
