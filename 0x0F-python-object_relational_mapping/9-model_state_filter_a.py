e87bd9ecb3a1cb75169eac93abc841562a4485c9
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:26:57 +0000

    Filter state that contans a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Fetch all that contains a"""
 
 import sqlalchemy
 from sqlalchemy import create_engine
@@ -6,21 +7,22 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-us = argv[1]
-pw = argv[2]
-db = argv[3]
+if __name__ == "__main__":
+    us = argv[1]
+    pw = argv[2]
+    db = argv[3]
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(us, pw, db), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(us, pw, db), pool_pre_ping=True)
 
-Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
-session = Session()
-all_state = session.query(State).filter(State.name.like("%a%")).all()
-if all_state:
-    for state in all_state:
-        print("{}: {}".format(state.id, state.name))
-else:
-    print()
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    all_state = session.query(State).filter(State.name.like("%a%")).all()
+    if all_state:
+        for state in all_state:
+            print("{}: {}".format(state.id, state.name))
+    else:
+        print()
 
-session.close()
+    session.close()
