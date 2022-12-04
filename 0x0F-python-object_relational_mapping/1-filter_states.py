0eb93b6f04ec26fed2c030b93aaafbac15d25936
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:08:16 +0000

    Fix error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """script tpo filter states"""
+
 from sys import argv
 import MySQLdb
 
@@ -7,8 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], password=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE
-                BINARY 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%'")
     states = cur.fetchall()
 
     for state in states:
