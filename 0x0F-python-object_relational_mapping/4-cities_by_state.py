3b328a8b0a529dcef1322bcbb4a83711a1b11fad
apha-pesso <mutiuayo99@gmail.com>
2022-12-04 23:16:35 +0000

    Add description and backslash

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Script to get cities by state"""
 from sys import argv
 import MySQLdb
 
@@ -6,9 +7,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], password=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name,
-                states.name FROM cities
-                JOIN states ON cities.state_id=states.id
+    cur.execute("SELECT cities.id, cities.name, \
+                states.name FROM cities \
+                JOIN states ON cities.state_id=states.id \
                 ORDER BY cities.id ASC")
 
     [print(city) for city in cur.fetchall()]
