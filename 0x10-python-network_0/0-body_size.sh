6ee2ec0538d8154e6ea5a1c9038527f116c940f0
apha-pesso <mutiuayo99@gmail.com>
2022-12-15 21:00:51 +0000

    Curl methods

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # display content size using curl
-# curl -sI "$1" | grep -i content-length | cut -d " " -f2
-# curl -s "$1" | wc -c
-curl -s "$1" | wc -c
+curl -sI "$1" | grep -i content-length | cut -d " " -f2
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# curl method
+curl -s -X DELETE "$1"
