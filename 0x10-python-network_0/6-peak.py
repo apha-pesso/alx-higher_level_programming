75f6c0c11c37e4b123c89d3631e8f0a2c2f92b9c
apha-pesso <mutiuayo99@gmail.com>
2022-12-16 07:26:33 +0000

    check

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,8 @@
 
 def find_peak(list_of_integers):
     """Find peak"""
+    if not list_of_integers:
+        return None
     max_num = list_of_integers[0]
     for i in range(1, len(list_of_integers)):
         if list_of_integers[i] > max_num:
