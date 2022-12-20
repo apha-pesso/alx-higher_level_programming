47a8ea9e279352332e45f4eeaa268c523167e3ba
apha-pesso <mutiuayo99@gmail.com>
2022-12-20 22:02:21 +0000

    Fetch url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
+        content = req.read()
+        utf_content = content.decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf-8 content: {}".format(utf_content))
