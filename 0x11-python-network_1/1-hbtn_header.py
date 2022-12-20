1d8fe186dd9df939e47ae39a4fd9fed09f990096
apha-pesso <mutiuayo99@gmail.com>
2022-12-20 22:35:59 +0000

    Fix sys requirement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Moule to fetch status"""
+"""Moule to fetch response header using usrllib"""
 # Python script that fetches https://alx-intranet.hbtn.io/status
 
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
+    from sys import argv
+    with urllib.request.urlopen(argv[1]) as req:
         head = req.headers
     dict(head)
     print(head['X-Request-Id'])
