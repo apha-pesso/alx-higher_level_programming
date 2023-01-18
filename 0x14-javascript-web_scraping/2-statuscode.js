df200dfc4ef8d8fca851ee0c135d29b6606ef2c4
apha-pesso <mutiuayo99@gmail.com>
2023-01-18 15:18:29 -0800

    Fix error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, (error, response, body) => {
-  console.log('error:', error);
+  if (error) console.log('error:', error);
   console.log('code:', response.statusCode);
 });
