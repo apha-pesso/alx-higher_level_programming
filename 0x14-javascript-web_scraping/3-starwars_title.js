95b52907eecd16fb1b47e213530e1cf89c4e86c7
apha-pesso <mutiuayo99@gmail.com>
2023-01-18 15:29:54 -0800

    Change strings to single quote

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 // with the given integer
 
 const request = require('request');
-const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (error) console.log('error:', error);
   console.log(body.title);
