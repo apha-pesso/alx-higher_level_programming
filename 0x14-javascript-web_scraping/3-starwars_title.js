3631a113176516fa104fb7803269749eb7da4850
apha-pesso <mutiuayo99@gmail.com>
2023-01-18 15:37:03 -0800

    Parse to json

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,5 +6,6 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (error) console.log('error:', error);
-  console.log(body.title);
+  const res = JSON.parse(body);
+  console.log(res.title);
 });
