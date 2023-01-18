36fea05f542e20994124f1862ceb5046f7dd4e1a
apha-pesso <mutiuayo99@gmail.com>
2023-01-18 15:27:45 -0800

    Get the title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints the title of a Star Wars movie
+// with the given integer
+
+const request = require('request');
+const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+request(url, (error, response, body) => {
+  if (error) console.log('error:', error);
+  console.log(body.title);
+});
